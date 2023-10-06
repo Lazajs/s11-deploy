8e4233e6bfc89cad53a42139333eb053ee9aa8ce
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-10-06 20:43:32 -0300

    fix: cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 app.disable('x-powered-by')
+app.use(cors())
 app.use(express.json())
 
 app.get('/api/example', (req, res) => {
