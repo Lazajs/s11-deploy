f3e258553e0f24552dc97e82ff882514065ba1ab
Enzo Gonzalez <ghrelin00@gmail.com>
2023-10-13 17:16:19 -0300

    feat: update server configuration and add route for real-time notifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,57 @@
 import express from 'express'
+import http from 'http'
 import cors from 'cors'
 import { config } from 'dotenv'
 import connect from './db/connect'
 import auth from './routes/auth'
+import notification from './routes/notification'
+
+import {createNotification} from './controllers/Notification/createNotification'
 
 config()
+
+import { configureSocketIO } from './sockets/socket'
+
 const app = express()
 app.disable('x-powered-by')
-app.use(cors())
+
+const httpServer = http.createServer(app)
+
+configureSocketIO(httpServer)
+
+const databaseURI = process.env.MONGOOSE_URI
+const frontEndURL = process.env.FRONT_END_URL
+
+if (!databaseURI || !frontEndURL) {
+  throw new Error('DATABASE_URI/FRONT_END_URL environment variable are not set.')
+}
+
+const corsOptions: cors.CorsOptions = {
+  origin: [frontEndURL],
+  methods: 'GET,POST',
+  credentials: true,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use('/api/auth', auth)
+app.use('/api/notification', notification)
 
 const { PORT } = process.env
 const port = PORT ?? 3001
 
-// connect().then(() => {
-//   console.log('Connected to MongoDB')
-// }).catch((err) => {
-//   console.log(err)
-// })
+const start = async () => {
+  try {
+    await connect(databaseURI)
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+    httpServer.listen(port, () => {
+      console.log(`Server is running on port ${port}`)
+    })
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
