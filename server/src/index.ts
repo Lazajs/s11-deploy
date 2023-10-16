bc184d2a07803617ae2153fc1c93e03898137f17
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-10-16 17:27:01 -0500

    Authgoogle

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,57 +1,53 @@
-import express from 'express'
-import http from 'http'
-import cors from 'cors'
-import { config } from 'dotenv'
-import connect from './db/connect'
-import auth from './routes/auth'
-import notification from './routes/notification'
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { PORT } from "./utils/config";
+import connect from "./db/connect";
+import auth from "./routes/auth";
+import notification from "./routes/notification";
 
-import {createNotification} from './controllers/Notification/createNotification'
+import { createNotification } from "./controllers/Notification/createNotification";
 
-config()
+import { configureSocketIO } from "./sockets/socket";
 
-import { configureSocketIO } from './sockets/socket'
+const app = express();
+app.disable("x-powered-by");
 
-const app = express()
-app.disable('x-powered-by')
+const httpServer = http.createServer(app);
 
-const httpServer = http.createServer(app)
+configureSocketIO(httpServer);
 
-configureSocketIO(httpServer)
-
-const databaseURI = process.env.MONGOOSE_URI
-const frontEndURL = process.env.FRONT_END_URL
+const databaseURI = process.env.MONGOOSE_URI;
+const frontEndURL = process.env.FRONT_END_URL;
 
 if (!databaseURI || !frontEndURL) {
-  throw new Error('DATABASE_URI/FRONT_END_URL environment variable are not set.')
+  throw new Error(
+    "DATABASE_URI/FRONT_END_URL environment variable are not set."
+  );
 }
 
 const corsOptions: cors.CorsOptions = {
   origin: [frontEndURL],
-  methods: 'GET,POST',
+  methods: "GET,POST",
   credentials: true,
-}
+};
 
-app.use(cors(corsOptions))
-app.use(express.json())
+app.use(cors(corsOptions));
+app.use(express.json());
 
-app.use('/api/auth', auth)
-app.use('/api/notification', notification)
-
-const { PORT } = process.env
-const port = PORT ?? 3001
+app.use("/api/auth", auth);
+app.use("/api/notification", notification);
 
 const start = async () => {
   try {
-    await connect(databaseURI)
+    await connect(databaseURI);
 
-    httpServer.listen(port, () => {
-      console.log(`Server is running on port ${port}`)
-    })
-
+    httpServer.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-start()
+start();
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,22 @@
-import { Router } from 'express'
-import { AuthController } from '../controllers/auth'
+import { Router } from "express";
+import { AuthController } from "../controllers/auth";
+import passport from "passport";
 
-const router = Router()
+const router = Router();
 
-router.post('/signup', AuthController.signup)
-router.post('/signin', AuthController.signin)
+router.post("/signup", AuthController.signup);
+router.post("/signin", AuthController.signin);
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
 
-export default router
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/home",
+    failureRedirect: "/auth/failure",
+  })
+);
+
+export default router;
