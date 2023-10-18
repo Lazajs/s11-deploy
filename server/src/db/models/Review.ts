67c7b9643330fe23dec67e647da92deea17d3810
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-10-18 11:58:33 -0500

    Reviews

---
+++ server/src/db/models/Review.ts
@@ -0,0 +1,33 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+
+export interface IReview extends Document {
+  score: number;
+  text: string;
+  eventId?: string;
+  organizerId?: string;
+}
+
+const reviewSchema = new Schema<IReview>({
+  score: {
+    type: Number,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  eventId: {
+    type: Schema.Types.ObjectId,
+    ref: "Event",
+    required: true,
+  },
+  organizerId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const ReviewModel: Model<IReview> = mongoose.model("Review", reviewSchema);
+
+export default ReviewModel;
--- server/src/types.ts
+++ server/src/types.ts
@@ -1,25 +1,27 @@
-import { type Document } from 'mongoose'
+import { type Document } from "mongoose";
 
 export interface DatabaseUser extends Document {
-  email: string
-  name: string
-  passwordHash: string
-  nextEvents: unknown[] // TODO
-  reviews: unknown[] // TODO
-  ownEvents: unknown[] // TODO
-  rating: number
-  image: string
-  birthdate: string
+  email: string;
+  name: string;
+  passwordHash: string;
+  nextEvents: unknown[]; // TODO
+  reviews: unknown[]; // TODO
+  ownEvents: unknown[]; // TODO
+  rating: number;
+  image: string;
+  birthdate: string;
+  googleId: string;
 }
 
 export interface IUser {
-  email: string
-  name: string
-  password: string
-  birthdate: string
-  image: string
-  nextEvents?: unknown[] // TODO
-  reviews?: unknown[] // TODO
-  ownEvents?: unknown[] // TODO
-  rating?: number
+  email: string;
+  name: string;
+  password: string;
+  birthdate: string;
+  image: string;
+  nextEvents?: unknown[]; // TODO
+  reviews?: unknown[]; // TODO
+  ownEvents?: unknown[]; // TODO
+  rating?: number;
+  googleId?: string;
 }
