5e45d5b747c3516a1c1da55a81866173bbc773a9
Faradar <tomi_compuamste@hotmail.com>
2023-10-23 23:34:44 -0300

    feat: add static login component

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState } from 'react';
 import Link from 'next/link';
+import { Icon } from '@iconify/react';
 
 function LoginPage() {
   const [formData, setFormData] = useState({
@@ -22,34 +23,49 @@ function LoginPage() {
   }
 
   return (
-    <main className="flex flex-col items-center mt-10">
-      <div className="flex flex-col items-center w-[300px] xl:w-[800px] xl:h-[594px] xl:border-[6px] border-fifth rounded-[20px]">
-        {/* Form */}
-        <form onSubmit={handleSubmit} className="w-[330px]">
-          {/* Logo */}
-          <h1 className="border border-2 rounded-2xl outline-none px-4 py-2 mb-16 mx-auto w-[90%] xl:w-[100%] xl:text-[20px] xl:mt-12">
-            Logo
+    <main className="flex justify-center">
+      <div className="w-[304px] flex flex-col items-center justify-center py-8">
+        <div className="flex flex-row-reverse w-full">
+          <Icon icon="ph:x" className="text-[#54595F] w-[26px] h-[26px]" />
+        </div>
+        {/* Logo */}
+        <div className="mt-10">
+          <img src="/login/logo.svg" alt="Logo" className="w-[131px]" />
+        </div>
+        {/* Title */}
+        <div className="mt-6 mb-6 w-full">
+          <h1 className="font-bold text-[27px] text-left">
+            ¿Listo para empezar?
           </h1>
-          {/* Email */}
-          <div className="mb-6 xl:mt-20">
-            <div className="flex items-center">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email || ''}
-                onChange={handleChange}
-                required
-                placeholder="Email"
-                className="border border-2 rounded-2xl outline-none px-4 py-2 mx-auto w-[100%] xl:text-[20px] xl:mb-4"
-              />
-            </div>
-          </div>
+        </div>
 
-          {/* Password */}
-          <div className="mb-16 xl:mb-8">
-            <div className="flex items-center">
-              <div className="border border-2 rounded-2xl outline-none px-4 py-2 mx-auto w-[100%] xl:text-[20px]">
+        <div>
+          {/* Form */}
+          <form onSubmit={handleSubmit}>
+            {/* Email */}
+            <div className="mb-6">
+              <div className="flex flex-col font-medium">
+                <label htmlFor="email" className="text-[11px] mb-2.5">
+                  Correo electronico
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email || ''}
+                  onChange={handleChange}
+                  required
+                  placeholder="juan@mail.com"
+                  className="border border-[#1E1E1E] rounded-md outline-none px-4 py-2 w-[303px] h-[38px] text-[9.5px] placeholder:text-[#808080] placeholder:font-normal"
+                />
+              </div>
+            </div>
+            {/* Password */}
+            <div>
+              <div className="flex flex-col font-medium">
+                <label htmlFor="password" className="text-[11px] mb-2.5">
+                  Contraseña
+                </label>
                 <input
                   type="password"
                   id="password"
@@ -57,40 +73,48 @@ function LoginPage() {
                   value={formData.password || ''}
                   onChange={handleChange}
                   required
-                  placeholder="Password"
-                  className="w-full mr-2 outline-none"
+                  placeholder="Contraseña"
+                  className="border border-[#ADADAD] rounded-md outline-none px-4 py-2 w-[303px] h-[38px] text-[9.5px] placeholder:text-[#808080] placeholder:font-normal"
                 />
               </div>
             </div>
-          </div>
-
-          {/* Submit */}
-          <div className="xl:w-[477px]">
-            <button
-              type="submit"
-              className="border border-2 rounded-2xl outline-none px-4 py-2 mb-8 mx-auto w-[100%] xl:text-[20px] xl:w-[330px]"
-            >
-              Continue
-            </button>
-          </div>
-        </form>
-
-        {/* Google */}
-        <div className="mt-12">
-          <Link
-            href="/"
-            className="flex items-center justify-start button border border-sixth border-2 rounded-2xl px-4 space-x-6 w-[330px] h-[41px] xl:hidden"
-          >
-            <p>Continuar con Google</p>
-          </Link>
+            {/* Forgot Password */}
+            <div className="font-medium text-[#3C76A6] text-[9.5px] text-right mt-1 mr-2">
+              <Link href="/login">Olvidé la contraseña</Link>
+            </div>
+            {/* Submit */}
+            <div className="mt-8 w-[303px] h-[36px]">
+              <button
+                type="submit"
+                className="border rounded-md bg-[#659DCB] outline-none w-full h-full text-white text-[11px]"
+              >
+                Iniciar sesión
+              </button>
+            </div>
+          </form>
         </div>
         {/* Guest */}
-        <Link
-          href="/"
-          className="flex items-center justify-start button border border-sixth border-2 rounded-2xl px-4 space-x-6 w-[330px] h-[41px] mt-4 xl:hidden"
-        >
-          ¿No tienes cuenta? <span className="underline">Regístrate</span>
-        </Link>
+        <div>
+          <Link
+            href="/login"
+            className="flex flex-col items-center justify-start my-4 text-[9px] text-[#8D8D8D]"
+          >
+            ¿Nuevo usuario?{' '}
+            <span className="text-[#3C76A6] font-medium">Registrate</span>
+          </Link>
+        </div>
+        {/* Google */}
+        <div>
+          <Link
+            href="/login"
+            className="flex items-center justify-center w-[35px]"
+          >
+            <Icon
+              icon="flat-color-icons:google"
+              className="w-[35px] h-[35px]"
+            />
+          </Link>
+        </div>
       </div>
     </main>
   );
